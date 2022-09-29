e65b9f717afcdcc4a96f40c75946f6c0fd66fe15
pkr187 <pankajroman187@gmail.com>
2022-09-29 20:05:19 +0530

    [pankaj] Add:FindLeapYear

---
+++ FindLeapYear/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FindLeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int year = 1994; year <= 2014; year++)
+            {
+                if (DateTime.IsLeapYear(year))
+                {
+                    Console.WriteLine("{0} is a leap year.", year);
+                    DateTime leapDay = new DateTime(year, 2, 29);
+                    DateTime nextYear = leapDay.AddYears(1);
+                    Console.WriteLine("   One year from {0} is {1}.", leapDay.ToString("d"), nextYear.ToString("d"));                  
+                }
+            }
+        }
+    }
+}
